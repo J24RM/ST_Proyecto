84fc8d144683da84e032d1cdeea73eb31c877426
Jesús Rodriguez <rodriguezmendozajesus8@gmail.com>
2025-09-20 22:40:23 -0600

    Se modifico los comentarios para cumplir con el estandar PEP8

--- pacman.py
+++ pacman.py
@@ -32,15 +32,18 @@ state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
 aim = vector(5, 0)
-pacman = vector(-40, -80) # Posicion inicial del jugador
-ghosts = [ # Posicion inicial y velocidad inicial de los fantasmas
+# Posicion inicial del jugador
+pacman = vector(-40, -80)
+# Posicion inicial y velocidad inicial de los fantasmas
+ghosts = [
     [vector(-180, 160), vector(20, 0)],
     [vector(-180, -160), vector(0, 20)],
     [vector(100, 160), vector(0, -20)],
     [vector(100, -160), vector(-20, 0)],
 ]
 # fmt: off
-tiles = [ # Forma del Tablero
+# Forma del Tablero
+tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
@@ -63,6 +66,7 @@ tiles = [ # Forma del Tablero
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
 # fmt: on
+
 
 def square(x, y):
     """Draw square using path at (x, y)."""
@@ -104,7 +108,8 @@ def valid(point):
 def world():
     """Draw world using path."""
     bgcolor('black')
-    path.color('blue') #color del camino
+    path.color('blue')
+    # Color del camino
 
     for index in range(len(tiles)):
         tile = tiles[index]
@@ -112,12 +117,12 @@ def world():
         if tile > 0:
             x = (index % 20) * 20 - 200
             y = 180 - (index // 20) * 20
-            square(x, y) 
+            square(x, y)
 
             if tile == 1:
                 path.up()
                 path.goto(x + 10, y + 10)
-                path.dot(5, 'green') # Color y tamanio de la comida
+                path.dot(5, 'green')
 
 
 def move():
@@ -141,13 +146,15 @@ def move():
 
     up()
     goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow') # Color y tamanio del pacman
+    dot(20, 'yellow')
+    # Color y tamanio del pacman
 
     for point, course in ghosts:
         if valid(point + course):
             point.move(course)
         else:
-            options = [ #Velocidad de los fantasmas
+            # Velocidad de los fantasmas
+            options = [
                 vector(20, 0),
                 vector(-20, 0),
                 vector(0, 20),
@@ -181,9 +188,10 @@ setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 writer.goto(160, 160)
-writer.color('white') 
+writer.color('white')
 writer.write(state['score'])
-listen()#Movimientos del jugadoe 
+listen()
+# Movimientos del jugador
 onkey(lambda: change(5, 0), 'Right')
 onkey(lambda: change(-5, 0), 'Left')
 onkey(lambda: change(0, 5), 'Up')
